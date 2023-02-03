865eda1e2952d35213b35234de6ddc63da8e46a7
Martin Koldovsky <mkoldov@gmail.com>
2023-02-03 10:20:53 +0100

    .

--- 02-checkpoint-gwlb-variables.tf
+++ 02-checkpoint-gwlb-variables.tf
@@ -29,3 +29,142 @@ variable "configuration_template" {
   description = "A name of a gateway configuration template in the automatic provisioning configuration."
   default = "gwlb-ASG-configuration"
 }
+
+// --- Check Point CloudGuard IaaS Security Gateways Auto Scaling Group Configuration ---
+
+variable "gateway_name" {
+  type = string
+  description = "The name tag of the Security Gateway instances. (optional)"
+  default = "Check-Point-Gateway-tf"
+}
+
+variable "gateway_instance_type" {
+  type = string
+  description = "The EC2 instance type for the Security Gateways."
+  default = "c5.xlarge"
+}
+module "validate_instance_type" {
+  source = "./modules/common/instance_type"
+
+  chkp_type = "gateway"
+  instance_type = var.gateway_instance_type
+}
+variable "minimum_group_size" {
+  type = number
+  description = "The minimal number of Security Gateways."
+  default = 2
+}
+variable "maximum_group_size" {
+  type = number
+  description = "The maximal number of Security Gateways."
+  default = 10
+}
+variable "gateway_version" {
+  type = string
+  description =  "The version and license to install on the Security Gateways."
+  default = "R80.40-BYOL"
+}
+module "validate_gateway_version" {
+  source = "./modules/common/version_license"
+
+  chkp_type = "gwlb_gw"
+  version_license = var.gateway_version
+}
+variable "gateway_password_hash" {
+  type = string
+  description = "(Optional) Admin user's password hash (use command 'openssl passwd -6 PASSWORD' to get the PASSWORD's hash)"
+}
+variable "gateway_SICKey" {
+  type = string
+  description = "The Secure Internal Communication key for trusted connection between Check Point components (at least 8 alphanumeric characters)"
+}
+
+variable "gateways_provision_address_type" {
+  type = string
+  description = "Determines if the gateways are provisioned using their private or public address"
+  default = "private"
+}
+
+variable "allocate_public_IP" {
+  type = bool
+  description = "Allocate an Elastic IP for security gateway."
+  default = false
+}
+
+resource "null_resource" "invalid_allocation" {
+  // Will fail if var.gateways_provision_address_type is public and var.allocate_public_IP is false
+  count = var.gateways_provision_address_type != "public" ? 0 : var.allocate_public_IP == true ? 0 : "Gateway's selected to be provisioned by public IP, but [allocate_public_IP] parameter is false"
+}
+
+variable "enable_cloudwatch" {
+  type = bool
+  description = "Report Check Point specific CloudWatch metrics."
+  default = false
+}
+
+variable "key_name" {
+  type = string
+  description = "The EC2 Key Pair name to allow SSH access to the instances"
+}
+variable "enable_volume_encryption" {
+  type = bool
+  description = "Encrypt Environment instances volume with default AWS KMS key"
+  default = true
+}
+variable "volume_size" {
+  type = number
+  description = "Root volume size (GB) - minimum 100"
+  default = 100
+}
+resource "null_resource" "volume_size_too_small" {
+  // Will fail if var.volume_size is less than 100
+  count = var.volume_size >= 100 ? 0 : "variable volume_size must be at least 100"
+}
+variable "volume_type" {
+  type = string
+  description = "General Purpose SSD Volume Type"
+  default = "gp3"
+}
+variable "admin_shell" {
+  type = string
+  description = "Set the admin shell to enable advanced command line configuration"
+  default = "/etc/cli.sh"
+}
+variable "enable_instance_connect" {
+  type = bool
+  description = "Enable SSH connection over AWS web console"
+  default = false
+}
+variable "disable_instance_termination" {
+  type = bool
+  description = "Prevents an instance from accidental termination"
+  default = false
+}
+variable "allow_upload_download" {
+  type = bool
+  description = "Automatically download Blade Contracts and other important data. Improve product experience by sending data to Check Point"
+  default = true
+}
+variable "management_deploy" {
+  type = bool
+  description = "Select 'false' to use an existing Security Management Server or to deploy one later and to ignore the other parameters of this section"
+  default = true
+}
+variable "gateways_addresses" {
+  type = string
+  description = "Allow gateways only from this network to communicate with the Security Management Server"
+}
+variable "management_password_hash" {
+  type = string
+  description = "(Optional) Admin user's password hash (use command 'openssl passwd -6 PASSWORD' to get the PASSWORD's hash)"
+  default = ""
+}
+variable "admin_cidr" {
+  type = string
+  description = "Allow web, ssh, and graphical clients only from this network to communicate with the Security Management Server"
+}
+variable "gateway_management" {
+  type = string
+  description = "Select 'Over the internet' if any of the gateways you wish to manage are not directly accessed via their private IP address."
+  default = "Locally managed"
+}
