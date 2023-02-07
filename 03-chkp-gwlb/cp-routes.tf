4e8e5bdef02ced9bb303b059ed86a2afd244d669
Martin Koldovsky <mkoldov@gmail.com>
2023-02-07 21:24:41 +0100

    .

--- 03-chkp-gwlb/cp-routes.tf
+++ 03-chkp-gwlb/cp-routes.tf
@@ -16,5 +16,14 @@ output "tgw_subnet_ids" {
 }
 
 output "azs" {
-    value = data.aws_availability_zones.available
+    value = data.aws_availability_zones.available.names
+}
+
+data "aws_subnet" "tgw_subnets" {
+  for_each = data.aws_subnet_ids.tgw_subnet_ids.ids
+  id       = each.value
+} 
+
+output "tgw_subnet_cidr_blocks" {
+  value = {for s in data.aws_subnet.tgw_subnets : s.availability_zone => s.cidr_block}
 }
