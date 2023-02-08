7cf5efe3476a9a43ecde136d090aa4aa01da7149
Martin Koldovsky <mkoldov@gmail.com>
2023-02-08 18:40:20 +0100

    .

--- 03-chkp-gwlb/r.tf
+++ 03-chkp-gwlb/r.tf
@@ -74,3 +74,81 @@ output "tgw-subnets" {
 output "tgw-route-tables" {
     value = data.aws_route_table.tgw
 }
+
+///
+
+data "aws_subnet_ids" "nat_subnet_ids" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "net-chkp-private-*"
+  }
+}
+
+data "aws_subnet" "nat_subnet" {
+  for_each = data.aws_subnet_ids.nat_subnet_ids.ids
+  id       = each.value
+} 
+
+data "aws_nat_gateway" "default" {
+  for_each = data.aws_subnet_ids.nat_subnet_ids.ids
+  subnet_id = each.value
+}
+
+locals {
+    nat_subnet_id_by_az = {for s in data.aws_subnet.nat_subnet :  s.availability_zone => s.id  }
+    nat_subnet_a = local.nat_subnet_id_by_az[local.azs[0]]
+    nat_subnet_b = local.nat_subnet_id_by_az[local.azs[1]]
+    nat_subnet_c = local.nat_subnet_id_by_az[local.azs[2]]
+    natgw_a = data.aws_nat_gateway.default[nat_subnet_a].id
+    natgw_b = data.aws_nat_gateway.default[nat_subnet_b].id
+    natgw_c = data.aws_nat_gateway.default[nat_subnet_c].id
+
+    nat_rt_a = data.aws_route_table.nat[local.nat_subnet_a].id
+    nat_rt_b = data.aws_route_table.nat[local.nat_subnet_b].id
+    nat_rt_c = data.aws_route_table.nat[local.nat_subnet_c].id
+}
+
+output "nat_subnet_a" {
+    value = local.nat_subnet_a
+}
+output "nat_subnet_b" {
+    value = local.nat_subnet_b
+}
+output "nat_subnet_c" {
+    value = local.nat_subnet_c
+}
+
+output "natgw_a" {
+    value = local.natgw_a
+}
+output "natgw_b" {
+    value = local.natgw_b
+}
+output "natgw_c" {
+    value = local.natgw_c
+}
+
+
+data "aws_route_table" "nat" {
+  for_each = data.aws_subnet_ids.nat_subnet_ids.ids
+  subnet_id = each.value
+}
+
+output "nat_rt_a" {
+    value = local.nat_rt_a
+}
+output "nat_rt_b" {
+    value = local.nat_rt_b
+}
+output "nat_rt_c" {
+    value = local.nat_rt_c
+}
+
+
+output "cmd_fw_on" {
+    value = "aws ec2 replace-route --route-table-id ${local.tgw_rt_a} --destination-cidr-block 0.0.0.0/0 --vpc-endpoint-id ${local.gwlbe_a}"
+}
+output "cmd_fw_off" {
+    value = "aws ec2 replace-route --route-table-id ${local.tgw_rt_a} --destination-cidr-block 0.0.0.0/0 --nat-gateway-id ${local.natgw_a}"
+}
