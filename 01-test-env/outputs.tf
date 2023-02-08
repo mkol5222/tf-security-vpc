0134e45c2cc7b312b89dc7788d41bd50132952d6
Martin Koldovsky <mkoldov@gmail.com>
2023-02-08 13:22:55 +0100

    .

--- 01-test-env/outputs.tf
+++ 01-test-env/outputs.tf
@@ -3,7 +3,15 @@
   value = module.launch_vpc.private_subnets_ids_list
 } */
 
-output "tgw_subnets_ids_list" {
+output "vpc_id" {
+  value = module.launch_vpc.vpc_id
+}
+
+output "region" {
+ value = "eu-central-1"
+}
+
+output "subnets_ids_list" {
   value = module.launch_vpc.tgw_subnets_ids_list
 }
 
@@ -15,6 +23,3 @@ output "chkp_gwlbe_subnets_ids_list" {
   value = [for gwlbe_subnet in aws_subnet.gwlbe_subnet : gwlbe_subnet.id]
 }
 
-output "vpc_id" {
-  value = module.launch_vpc.vpc_id
-}
