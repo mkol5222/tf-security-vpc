6768d86d185fc2d626ee8dacc53e60cbb16e5a1a
Martin Koldovsky <mkoldov@gmail.com>
2023-02-09 11:18:36 +0100

    publishing

--- _unused/03-instance-in-tgw-subnet.tf
+++
@@ -1,7 +0,0 @@
-
-module "instanceA" {
-  source = "./mymodules/ssm-host"
-    region = "eu-central-1"
-    vpc_id = module.launch_vpc.vpc_id
-    subnet_id = module.launch_vpc.tgw_subnets_ids_list[0]
-}
